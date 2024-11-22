246fca545090ce3d3ef6c0238e69d1c742852659
Carlos <cquintanasolis@gmail.com>
2024-11-22 14:42:21 +0100

    Manteniendo datos de login

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,39 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
-import { JournalPage } from '../journal/pages/JournalPage';
+import { JournalRoutes } from '../journal/routes/JournalRoutes';
+import { useDispatch, useSelector } from 'react-redux';
+import { CheckingAuth } from '../ui';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { FirebaseAuth } from '../firebase/config';
+import { login, logout } from '../store/auth';
 
 export const AppRouter = () => {
+	const { status } = useSelector((state) => state.authSlice);
+	const dispatch = useDispatch();
+
+	//* Mantener los datos del login (si sigues logeado) al recargar el navegador
+	useEffect(() => {
+		onAuthStateChanged(FirebaseAuth, async (user) => {
+			if (!user) return dispatch(logout());
+			const { uid, email, displayName, photoURL } = user;
+			dispatch(login({ uid, email, displayName, photoURL }));
+		}); // Es un obserbable
+	}, []);
+
+	if (status === 'checking') {
+		return <CheckingAuth />;
+	}
 	return (
 		<Routes>
-			<Route path="/auth/*" element={<AuthRoutes />} />
-			<Route path="/*" element={<JournalPage />} />
+			{status === 'authenticated' ? (
+				<Route path="/*" element={<JournalRoutes />} />
+			) : (
+				<Route path="/auth/*" element={<AuthRoutes />} />
+			)}
+			<Route path="/*" element={<Navigate to="/auth/login" />} />
+			{/* <Route path="/auth/*" element={<AuthRoutes />} />
+			<Route path="/*" element={<JournalPage />} /> */}
 		</Routes>
 	);
 };
